e743ab3f03e7905a1312dfba4c7c199a6e5dfc6a
Øistein Haugland <haugland.1193@gmail.com>
2017-03-06 08:28:06 +0100

    finished my_each_with index, started my_select

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -6,11 +6,63 @@ module Enumerable
 			yield(element)
 		end
 	end
+
+
+	def my_each_with_index
+		index = 0
+		for element in self
+			yield(element,index)
+			index+=1
+		end
+	end
+
+#hmm
+	def my_select(&block)
+		for element in self
+			yield(element)
+		end
+	end
 end
 
 
-test = [1,2,3,4,5]
+test = [5,1,20,150,9,5]
 
-test.my_each do |x|
-	p x
+#test.my_each_with_index do |x,y|
+	#puts y
+#end
+
+test.select do |x|
+	p x == 5
 end
+
+
+=begin
+
+	
+
+	def my_all?
+
+	end
+
+	def my_any?
+
+	end
+
+	def my_none?
+
+	end
+
+	def my_count
+
+	end
+
+	def my_map
+	end
+
+	def my_inject
+	end
+
+	def multiply_els
+	end	
+=end
+	
