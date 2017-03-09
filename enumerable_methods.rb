65c7c1289ff074b6e12546777fed527b01150251
Øistein Haugland <haugland.1193@gmail.com>
2017-03-09 21:11:35 +0100

    Created and refactored my_reduce. It works correctly without a block atm

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -87,9 +87,8 @@ module Enumerable
 		result
 	end
 
-	def my_count(parameter = (no_argument_passed = true;))
+	def my_count(parameter = (no_argument_passed = true))
 		count = 0
-		
 		if no_argument_passed && !block_given? 
 			for elements in self
 				count += 1
@@ -108,16 +107,80 @@ module Enumerable
 		count
 	end
 
+
+
+	#my_map! would be another method that changes the given array instead of a new one.
+	def my_map
+		return to_enum(:my_map) unless block_given?
+		mapped_array = []
+		for elements in self
+			mapped_array.push(yield(elements))
+		end
+		mapped_array
+	end
+
+
+	#Apparently the inject and reduce methods are aliases. There is no performance benefit to either.
+
+	def my_reduce(argument = (no_argument_passed = true), args = {})
+		sum = nil
+		
+#if a block is given -> for each element in the array, yield the block.
+
+		if no_argument_passed && block_given?	
+			result = []
+			for elements in self
+				result.push(yield(elements))
+			end
+			
+			return result
+		end
+
+
+		if (argument.is_a? (Integer)) && (args.is_a? (Symbol))  #If you pass an integer and a symbol
+			sum = argument
+			symbolCase = args		
+		elsif(argument.is_a? (Symbol)) && args.size == 0 #If you just pass a symbol.
+			sum = self[0]
+			symbolCase = argument
+		end 
+
+		#this works when no block is given
+		if !block_given?
+			self.my_each_with_index do |x,i|
+				next if i == 0
+				case symbolCase
+					when :+
+						sum += x
+					when :-
+						sum -= x
+					when :/
+						sum /= x
+					when :*
+						sum *= x
+					when :**
+						sum **=x
+				end
+
+			end	
+		return sum
+		end
+	end
+
 end #module end
 
 
 
-test = ["bob",52,"test",1,2,3,2]
-bob = test.my_count do |x|
-	x == 2
+test = [1,2,3,4,5]
+
+#bob = test.reduce(1, :+)
+per =  test.my_reduce do |x|
+	x += 5
 end
 
-p bob
+# p test
+#p bob
+p per
 
 
 =begin
@@ -125,11 +188,7 @@ p bob
 
 	
 
-	def my_map
-	end
 
-	def my_inject
-	end
 
 	def multiply_els
 	end	
